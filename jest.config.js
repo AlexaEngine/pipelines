d625388983212eb6f93be05cb699f3ae87263fff
AlexaEngine <sykestudent@gmail.com>
2024-09-02 17:28:27 -0400

    Setup Express app with Jest testing and GitHub Actions CI

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+    transform: {
+      "^.+\\.[tj]sx?$": "babel-jest" // Transform TypeScript and JavaScript files
+    },
+    testEnvironment: 'node',
+  };
+  
--- test/test.js
+++ test/test.js
@@ -1,19 +1,26 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
+import request from 'supertest';
+import app from '../index.js';
 
-const { expect } = chai;
-chai.use(chaiHttp);
+let server;  // Define a variable to hold the server instance
 
-const app = require('../index'); 
+beforeAll((done) => {
+  server = app.listen(4000, () => { // Start the server on a different port for testing
+    console.log('Server started for testing');
+    done();  // Signal Jest that setup is complete
+  });
+});
+
+afterAll((done) => {
+  server.close(() => {  // Close the server after tests
+    console.log('Server closed after testing');
+    done();  // Signal Jest that teardown is complete
+  });
+});
 
 describe('GET /', () => {
-  it('should return "Hello World"', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.equal('Hello World');
-        done();
-      });
-  });
+  it('should return "Hello World"', async () => {
+    const res = await request(server).get('/');  // Use server instance here
+    expect(res.statusCode).toBe(200);  // Expect HTTP status 200
+    expect(res.text).toBe('Hello World');  // Expect response text
+  }, 10000);  // Increase timeout to 10 seconds for this test
 });
